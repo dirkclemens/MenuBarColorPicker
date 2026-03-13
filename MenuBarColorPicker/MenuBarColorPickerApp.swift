5e8b489f5527d5572e2e37c1f6fb07cfb768f323
Dirk Clemens <git@adcore.de>
2026-03-13 17:43:13 +0100

    Initial Commit

---
+++ MenuBarColorPicker/MenuBarColorPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuBarColorPickerApp.swift
+//  MenuBarColorPicker
+//
+//  Created by Dirk Clemens on 13.03.26.
+//
+
+import SwiftUI
+
+@main
+struct MenuBarColorPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
